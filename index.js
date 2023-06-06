1036e221fae1ab41b735377b85294aa0558a5deb
brooks42 <britainsux@gmail.com>
2023-06-05 23:22:32 -0700

    add express dependency to pass DO health check

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var Discord = require("discord.js");
-const Servo = require('./lib/bot');
+var Discord = require("discord.js")
+const Servo = require('./lib/bot')
 require('dotenv').config()
 
+const express = require('express')
+const app = express()
+const port = 3000
+
 if (!process.env.DISCORD_TOKEN) {
-  console.log('Error: Specify DISCORD_TOKEN in environment');
-  process.exit(1);
+  console.log('Error: Specify DISCORD_TOKEN in environment')
+  process.exit(1)
 }
 
-new Servo(process.env.DISCORD_TOKEN);
+new Servo(process.env.DISCORD_TOKEN)
+
+// express just to pass health check
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
