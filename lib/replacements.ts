694dfe16491a604d1d869e13857c67759b36fc5b
brooks42 <britainsux@gmail.com>
2023-11-14 21:44:12 -0800

    add more replacements

--- lib/replacements.ts
+++ lib/replacements.ts
@@ -12,3 +12,89 @@ replacements['poke ball'] = 'Poké Ball'
 replacements['nidoran'] = 'Nidoran♂'
 replacements['nidoranm'] = 'Nidoran♂'
 replacements['nidoranf'] = 'Nidoran♀'
+
+// poke flute
+replacements['poke flute'] = 'Poké Flute'
+replacements['poke flute'] = 'Poké Flute'
+
+// guard spec.
+replacements['guard spec'] = 'Guard Spec.'
+replacements['x-attack'] = 'X Attack'
+replacements['x-defense'] = 'X Defense'
+replacements['xattack'] = 'X Attack'
+replacements['xdefense'] = 'X Defense'
+
+// pokemon mansion
+replacements['pokemon mansion'] = 'Pokémon Mansion'
+
+// pokemon mansion
+replacements['pokemon center'] = 'Pokémon Center'
+
+// pokemon storage system
+replacements['pokemon storage system'] = 'Pokémon Storage System'
+replacements['pokemon storage'] = 'Pokémon Storage System'
+
+// diglett's cave
+replacements['digletts cave'] = "Diglett's Cave"
+
+// dragons den
+replacements['dragons den'] = "Dragon's Den"
+
+// sylph co
+replacements['sylph co'] = 'Sylph Co.'
+
+// bill's lands
+replacements['bills grassland'] = "Bill's Grassland"
+replacements['bills coastline'] = "Bill's Coastline"
+replacements['bills marsh'] = "Bill's Marsh"
+replacements['bills volcano'] = "Bill's Volcano"
+replacements['bills rainforest'] = "Bill's Rainforest"
+
+// mounts
+replacements['mt mortar'] = 'Mt. Mortar'
+replacements['mount mortar'] = 'Mt. Mortar'
+replacements['mt moon'] = 'Mt. Moon'
+replacements['mount moon'] = 'Mt. Moon'
+replacements['mt silver'] = 'Mt. Silver'
+replacements['mount silver'] = 'Mt. Silver'
+
+// ss anne
+replacements['ss anne'] = 'S.S. Anne'
+
+// stones
+replacements['moonstone'] = 'Moon Stone'
+replacements['thunderstone'] = 'Thunder Stone'
+replacements['firestone'] = 'Fire Stone'
+replacements['waterstone'] = 'Water Stone'
+replacements['leafstone'] = 'Leaf Stone'
+
+// mega kick
+replacements['megakick'] = 'Mega Kick'
+
+// ho-oh
+replacements['hooh'] = 'Ho-oh'
+
+// snubbull
+replacements['snubbul'] = 'Snubbull'
+replacements['snubul'] = 'Snubbull'
+replacements['snubull'] = 'Snubbull'
+
+// forretress
+replacements['fortress'] = 'Forretress'
+replacements['foretress'] = 'Forretress'
+
+// politoed
+replacements['politoad'] = 'politoed'
+
+// flaaffy
+replacements['flaffy'] = 'flaaffy'
+
+// electabuzz
+replacements['elektabuzz'] = 'electabuzz'
+
+// mr. mime
+replacements['mr mime'] = 'mr. mime'
+
+// farfetch'd
+replacements['farfetchd'] = "farfetch'd"
+replacements['farfetched'] = "farfetch'd"
